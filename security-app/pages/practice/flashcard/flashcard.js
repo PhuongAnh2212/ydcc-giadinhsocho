78d29b82c54a40db407e1f59c2075af19c1bfb95
Lê Thị Hà Chi <amuchan134@gmail.com>
2024-04-13 16:41:56 +0700

    Add image + explaination to flashcards

--- security-app/pages/practice/flashcard/flashcard.js
+++ security-app/pages/practice/flashcard/flashcard.js
@@ -2,35 +2,40 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import FlashcardList from './fc_list/flashcardList'
 import styles from './flashcard.styles';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // *Gets bug* Omg DnD moment (Roll nat 1 on a perception check)
 
 
 const Flashcard = ({ navigation }) => {
     const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARD)
+    const [current_tab, setCurrent_tab]= useState(0)
   return (
     <View style={styles.page_container}> 
+        <View style={styles.statusbar}></View>
 
-        {flashcards.map(flashcard => {
-                return <FlashcardList flashcard = {flashcard} key = {flashcard.id}
-                />
-            })}
-        <View role='button' accessible={true} onTouchEnd={()=>{
-        setFlip(!flip)    
-        }}>
-        <Text> 
-            flip
-        </Text>
-        
-    </View>
 
-    <View role='button' accessible={true} onTouchEnd={()=>{
-        setFlip(!flip)    
-        }}>
-        <Text> 
-            next
+        <FlashcardList flashcard = {flashcards[current_tab]} key = {flashcards[current_tab].id}/>
+
+
+        <View style={styles.navigate}>
+        <View style={[styles.selectButton,styles.back]} role='button' accessible={true} onTouchEnd={()=>{
+            current_tab <= 0? setCurrent_tab(current_tab):setCurrent_tab(current_tab-1)}}>
+            <Text style = {[styles.button_des, {
+                color:"#FF7F1E"
+            }]}> 
+                Back
+            </Text>
+            
+        </View>
+        <View style={[styles.selectButton,styles.next]} role='button' accessible={true} onTouchEnd={()=>{
+            current_tab >= flashcards.length-1? setCurrent_tab(current_tab):setCurrent_tab(current_tab+1)}}>
+        <Text style={[styles.button_des, {
+                color:"#FFFFFF"
+            }]}> 
+            Next
         </Text>
-        
+        </View>
+
     </View>
 
     </View>
@@ -40,22 +45,21 @@ const Flashcard = ({ navigation }) => {
 
 
 //Data
-const SAMPLE_FLASHCARD = [
+export const SAMPLE_FLASHCARD = [
     {
         id: 1,
         question: 'Hỏi đi',
-        answer: 'dap an'
+        answer: 'dap an',
     },
     {
         id: 2,
-        question: 'Hỏi đi 2 ',
-        answer: 'dap an 2'
+        question: 'Hỏi đi 2',
+        answer: 'dap an 2',
     },
     {
         id: 3,
         question: 'Hỏi đi 3',
-        answer: 'dap an 3'
+        answer: 'dap an 3',
     }
 ]
-
 export default Flashcard;
--- security-app/pages/practice/flashcard/flashcard.styles.js
+++ security-app/pages/practice/flashcard/flashcard.styles.js
@@ -2,43 +2,90 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     page_container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#EAF7FF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EAF7FF',
+    gap:100
     },
-    title: {
-      fontSize: 24,
-      marginBottom: 20,
-      color: 'black'
+    navigate:{
+        flexDirection:"row",
+        gap:50,
+    }
+    ,
+    selectButton:{
+        width:150,
+        height:70,
+        justifyContent: 'center',
+        alignItems: 'center',  
+        borderRadius:20,
     },
-    card:{
+    back:{
+        backgroundColor:"#FECE00",
+    },
+    next:{
+        backgroundColor:"#E2698F",
+        },
+    button_des: {
+      fontSize: 30,
+      fontWeight:500,
+    },
+    qna:{
+        position:'absolute',
         flex:1,
+        // backgroundColor: '#E2698F',
+        height:500,
+        alignContent:'flex-start',
+        justifyContent:'flex-start',
+        flexDirection:'column'
+    }
+    ,
+    card:{
         backgroundColor:'#0C69F1',
         justifyContent:'center',
         alignContent:'center',
         alignItems:'center',
-        transformStyle: 'preserve-3d',
         borderRadius:20,
-        width: 300,
-        backfaceVisibility:"hidden",
-
-    },
+        height:300,
+        width:350,
+        borderColor:"#0C69F1",
+        borderWidth:20,
+        overflow:'hidden',},
     flipped:{
-        
-        transform: "rotateY(180deg)",
-        backgroundColor:'#9B2626',
-        backfaceVisibility:"hidden",
+        justifyContent:'center',
+        alignContent:'center',
+        alignItems:'center',
+        borderRadius:20,
+        height:300,
+        width:350,
+        overflow:'hidden',
+        backgroundColor:'#FFFFFF',
+        borderColor:"#0C69F1",
+        borderWidth:20,
+        objectFit:'scale-down'
     },
+    question:{
+        fontSize: 45,
+        fontWeight:500,
+        color: "#FFFFFF",
+        },
+    answer:{
+        justifyContent:'center',
+        alignContent:'center',
+        alignItems:'center',
+        paddingTop:20
+    },
+    explain:{
+        fontSize: 30,
+        fontWeight:500,
+        color: "#0F1113",
+        },
+    img:{
+        maxWidth:400,
+        objectFit:'contain'
 
-    front:{
-        padding:"1%",
-        backfaceVisibility:"hidden",
-    },
-    back:{
-        backfaceVisibility:"hidden",
-        transform: "rotateY(180deg)"
     }
-  });
+
+});
 
 export default styles;
